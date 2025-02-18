3d6609c1b1452a0e426ab8127c6996d10dc883a8
aein <aaeinnor11@gmail.com>
2025-02-18 18:02:07 +0800

    Added draw_move() to cell.py

--- cell.py
+++ cell.py
@@ -14,6 +14,8 @@ class Cell:
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -31,3 +33,19 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            color = "red"
+        color = "gray"
+
+        center1 = abs(self._x2 - self._x1) // 2
+        x1 = center1 + self._x1
+        y1 = center1 + self._y1
+
+        center2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x2 = center2 + to_cell._x1
+        y2 = center2 + to_cell._y1
+
+        line = Line(Point(x1, y1), Point(x2, y2))
+        self._win.draw_line(line, fill_color=color)
--- main.py
+++ main.py
@@ -10,6 +10,26 @@ def main():
     cell.has_right_wall = False
     cell.draw(50, 50, 100, 100)
 
+    cell1 = Cell(win)
+    cell1.has_left_wall = False
+    cell1.has_bottom_wall = False
+    cell1.draw(100, 50, 150, 100)
+
+    cell.draw_move(cell1)
+
+    cell2 = Cell(win)
+    cell2.has_top_wall = False
+    cell2.has_right_wall = False
+    cell2.draw(100, 100, 150, 150)
+
+    cell1.draw_move(cell2)
+
+    cell3 = Cell(win)
+    cell3.has_left_wall = False
+    cell3.draw(150, 100, 200, 150)
+
+    cell2.draw_move(cell3)
+
     win.wait_for_close()
 
 
